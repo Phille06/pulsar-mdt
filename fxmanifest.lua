416e9a54c2d9afa53278d2da52354c193467e27e
Artmines <arturowells@gmail.com>
2026-04-28 05:43:05 -0230

    feat(init):Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+name("Sandbox RP Framework MDT")
+description("Mobile Data Terminal Written For Sandbox RP Framework")
+author("Dr Nick")
+lua54("yes")
+fx_version("cerulean")
+game("gta5")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+server_script("@oxmysql/lib/MySQL.lua")
+
+client_scripts({
+    "client/**/*.lua"
+})
+
+server_scripts({
+    '@oxmysql/lib/MySQL.lua',
+    "server/**/*.lua"
+})
+
+shared_scripts({
+    "shared/*.lua",
+})
+
+ui_page("ui/dist/index.html")
+
+files({ "ui/dist/index.html", "ui/dist/*.js" })
